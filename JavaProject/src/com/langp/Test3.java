0f1a0f330fd905c64d6bed0e5c32c2c06551d0e2
SHALINI KUMARI <SHALINI KUMARI@SHALINI>
2018-01-13 08:53:12 -1100

    Added Output in Test3

--- JavaProject/src/com/langp/Test3.java
+++ JavaProject/src/com/langp/Test3.java
@@ -4,12 +4,12 @@ public class Test3 {
 
 	public static void main(String[] args) {
 
+		System.out.println("MAIN START");
 		System.out.println("\"Hello\"");
 		String theFirst = "Java Programming";
 		String ROM = "\"" + theFirst + "\"";
-		
-		System.out.println(ROM);
-		
+
+		System.out.println("MAIN END");
 	}
 
 }
